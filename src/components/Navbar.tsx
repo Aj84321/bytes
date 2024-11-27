769b1057e7647e5159e387600973060c7c4257e4
anandcodexo <149761038+anandcodexo@users.noreply.github.com>
2024-11-27 17:27:37 +0530

    c

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0 font-bold text-2xl">
-            Gokhana
+          Bytes
           </div>
           
           <div className="hidden lg:block">
